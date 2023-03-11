1ea518de00079fed3bf10ccde5df794e44b3e895
Rusequalaizer <olympkirill@gmail.com>
2023-03-11 17:26:47 +0400

    Initial commit

---
+++ ReadFromArduino/ReadFromArduino/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFromArduino
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
